2c2ecdc8e92ede3aa1e480c60f73e158ab2622be
manapa <manapa@ttu.ee>
2020-03-10 19:12:10 +0200

    TodoItem added to API

---
+++ Visual2020/DataContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Visual2020.Model;
+
+namespace Visual2020
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        public DbSet<ToDoItem> ToDoItemList { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<ToDoItem>().Property(p => p.Id).HasIdentityOptions(2);
+            modelBuilder.Entity<ToDoItem>().HasData(
+                new ToDoItem
+                {
+                    Id = 1,
+                    Name = "Finish project",
+                    Description = "gfruwokp",
+                    DueDate = DateTime.Now,
+                    Status = false,
+                });
+        }
+    }
+}
---
+++ Visual2020/Model/ToDoItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+
+namespace Visual2020.Model
+{
+    public class ToDoItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public bool Status { get; set; }
+    }
+}
